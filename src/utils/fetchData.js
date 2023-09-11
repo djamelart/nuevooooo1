cbdf1ecd8011e33191ae71c79a76ef9db45c2f07
Djamel <pruevadjamel2020@hotmail.com>
2023-09-11 23:55:31 +0000

    iiiiiiii

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,39 +1,44 @@
-import axios from 'axios'
-const BASE_URL = "https://bckendnuevo.onrender.com" 
+import axios from 'axios';
 
+// Define la variable BASE_URL usando process.env.REACT_APP_BASE_URL
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+// Configura axios con la URL base
+const api = axios.create({
+    baseURL: BASE_URL,
+    // Otras configuraciones de axios, si es necesario
+});
 export const getDataAPI = async (url, token) => {
-    const res = await axios.get(`${BASE_URL}/api/${url}`, {
-        headers: { Authorization: token }
-    });
+    const res = await axios.get(`/api/${url}`, {
+        headers: { Authorization: token}
+    })
     return res;
-};
+}
 
 export const postDataAPI = async (url, post, token) => {
-    const res = await axios.post(`${BASE_URL}/api/${url}`, post, {
-        headers: { Authorization: token }
-    });
+    const res = await axios.post(`/api/${url}`, post, {
+        headers: { Authorization: token}
+    })
     return res;
-};
+}
 
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`${BASE_URL}/api/${url}`, post, {
-        headers: { Authorization: token }
-    });
+    const res = await axios.put(`/api/${url}`, post, {
+        headers: { Authorization: token}
+    })
     return res;
-};
+}
 
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`${BASE_URL}/api/${url}`, post, {
-        headers: { Authorization: token }
-    });
+    const res = await axios.patch(`/api/${url}`, post, {
+        headers: { Authorization: token}
+    })
     return res;
-};
+}
 
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`${BASE_URL}/api/${url}`, {
-        headers: { Authorization: token }
-    });
+    const res = await axios.delete(`/api/${url}`, {
+        headers: { Authorization: token}
+    })
     return res;
-};    
-
- 
+}

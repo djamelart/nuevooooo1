c73b64cd97de96258b4befde2c5e3a591bbce500
Djamel <pruevadjamel2020@hotmail.com>
2023-09-12 08:34:33 +0000

    gggg

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,36 +1,41 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const getDataAPI = async (url, token) => {
-    const res = await axios.get(`/api/${url}`, {
-        headers: { Authorization: token}
-    })
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const res = await axios.get(`${apiUrl}/api/${url}`, {
+        headers: { Authorization: token }
+    });
     return res;
 }
 
 export const postDataAPI = async (url, post, token) => {
-    const res = await axios.post(`/api/${url}`, post, {
-        headers: { Authorization: token}
-    })
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const res = await axios.post(`${apiUrl}/api/${url}`, post, {
+        headers: { Authorization: token }
+    });
     return res;
 }
 
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`/api/${url}`, post, {
-        headers: { Authorization: token}
-    })
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const res = await axios.put(`${apiUrl}/api/${url}`, post, {
+        headers: { Authorization: token }
+    });
     return res;
 }
 
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`/api/${url}`, post, {
-        headers: { Authorization: token}
-    })
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const res = await axios.patch(`${apiUrl}/api/${url}`, post, {
+        headers: { Authorization: token }
+    });
     return res;
 }
 
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`/api/${url}`, {
-        headers: { Authorization: token}
-    })
+    const apiUrl = process.env.REACT_APP_API_URL;
+    const res = await axios.delete(`${apiUrl}/api/${url}`, {
+        headers: { Authorization: token }
+    });
     return res;
 }

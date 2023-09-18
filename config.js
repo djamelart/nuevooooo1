268671d92faacf17dc3fe572774553fea7f08ee5
Djamel <pruevadjamel2020@hotmail.com>
2023-09-18 22:54:18 +0000

    uuuu

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+const API_URL = "https://bckendnuevo.onrender.com"; // Puedes cambiar esto según tus necesidades
+export default API_URL;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
  
-  const BASE_URL = process.env.REACT_APP_BASE_URL;
-  
+   
+  const BASE_URL = process.env.REACT_APP_API_URL || "https://bckendnuevo.onrender.com";
+export default BASE_URL;
 
 export const getDataAPI = async (url, token) => {
     const res = await axios.get(`${BASE_URL}api/${url}`, {
